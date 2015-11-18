985ad17d4945529e66850c3a0a8e61715e504b9a
Ian Copithorne <icopithorne@gmail.com>
2015-11-18 18:50:00 -0500

    Update GalileoClock.h

--- Lab3/galileoCode/GalileoClock.h
+++ Lab3/galileoCode/GalileoClock.h
@@ -1,7 +1,7 @@
 #ifndef GALILEO_CLOCK
 #define GALILEO_CLOCK
 
-int readClock(int file);
+int readClock(int file, unsigned char value[]);
 
 int setClock(int file, unsigned char day, unsigned char hour, unsigned char min, unsigned char sec);
 
